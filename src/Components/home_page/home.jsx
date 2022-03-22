81fb8d6a1d5aa23c34ede2f226dded9d915a3e05
SaharshPatel24 <srpatel56@myseneca.ca>
2022-03-22 13:52:52 -0400

    remove navigate

--- src/Components/home_page/home.jsx
+++ src/Components/home_page/home.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import './home.scss'
 import { Col, Row } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import logo from "../../images/IMG_0007-depositphotos-bgremover.png";
 
 const Home = () => {
-    const navigate = useNavigate();
 
 
     return (
