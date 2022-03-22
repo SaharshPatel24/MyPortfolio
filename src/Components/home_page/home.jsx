24d6021ed0e27e6943740c26200c6bffbcb728bc
SaharshPatel24 <srpatel56@myseneca.ca>
2022-03-22 12:34:56 -0400

    Initial Commit

---
+++ src/Components/home_page/home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './home.scss'
+import { Col, Row, Tooltip } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import logo from "../../images/IMG_0007-depositphotos-bgremover.png";
+
+const Home = () => {
+    const navigate = useNavigate();
+
+
+    return (
+        <Row className="home">
+
+            <Col>
+                <div className="speed-dial">
+                    <a className="toggle bg-bars"><span className="fa fa-bars"></span></a>
+                    <ul className="dials">
+                        <li><a className="bg-user" href="#"><span className="fa fa-user"></span></a></li>
+                        <li><a className="bg-code" href="#"><span className="fa fa-code"></span></a></li>
+                        <li><a className="bg-contact" href="#"><span className="fa fa-envelope"></span></a></li>
+                    </ul>
+                </div>
+            </Col>
+            <Row className="intro">
+                <h1>Saharsh Patel</h1>
+            </Row>
+            <Col className="image">
+                <img src={logo} alt="img" width={450} height={600} />
+            </Col>
+        </Row>
+
+    );
+};
+
+export default Home;
